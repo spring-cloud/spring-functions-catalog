6c079d36707cc575902704c6241fc6e3eda422c4
Artem Bilan <artem.bilan@broadcom.com>
2026-08-24 12:24:27 -0400

    Upgrade to Spring Java Format `0.0.48`
    Fixes: https://github.com/spring-cloud/spring-functions-catalog/pull/249
    
    Fix `AnalyticsConsumerParentTests` for Java Format check failure

--- consumer/spring-analytics-consumer/src/test/java/org/springframework/cloud/fn/consumer/analytics/AnalyticsConsumerParentTests.java
+++ consumer/spring-analytics-consumer/src/test/java/org/springframework/cloud/fn/consumer/analytics/AnalyticsConsumerParentTests.java
@@ -23,11 +23,12 @@ import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+@SpringBootTest(webEnvironment = WebEnvironment.NONE,
 		properties = { "management.wavefront.metrics.export.enabled=false" })
 @DirtiesContext
 public abstract class AnalyticsConsumerParentTests {
